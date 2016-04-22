bc3bfaee43fa5a2e42e41e06aff88441104977ee
gradyberryward <gward@brandeis.edu>
2016-04-22 13:27:53 -0500

    Quick fixes (JQuery, EditToLink)

--- war/static/js/init.js
+++ war/static/js/init.js
@@ -8,7 +8,7 @@
     var editorURL = "http://githubdemo-1285.appspot.com/edit";
     
     var pathToPage = window.location.href;
-	pathToPage.substring(pathToPage.indexOf("/", 7)+1)
+	pathToPage = pathToPage.substring(pathToPage.indexOf("/", 8)+1);
 	$("#edit-now-link").attr("href", editorURL + "/" + pathToPage);
 		
   });
