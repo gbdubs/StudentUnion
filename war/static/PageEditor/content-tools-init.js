0203138515a473ce6dadb27b56f1b6929d090c96
gradyberryward <gward@brandeis.edu>
2016-05-04 16:55:29 -0400

    Big and Small text editor

--- war/static/PageEditor/content-tools-init.js
+++ war/static/PageEditor/content-tools-init.js
@@ -14,10 +14,8 @@ window.addEventListener('load', function() {
 		new ContentTools.Style('Card', 'card', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		
 		new ContentTools.Style('Thin Text', 'light', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
-
 		new ContentTools.Style('Thinest Text', 'txt-super-light', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Boldest Text', 'txt-super-bold', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
-		
 		new ContentTools.Style('Smaller Text', 'txt-smaller-plz', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Bigger Text', 'txt-bigger-plz', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		
@@ -26,19 +24,19 @@ window.addEventListener('load', function() {
 		new ContentTools.Style('Text - Black', 'txt-brandeis-black', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Text - White', 'txt-brandeis-white', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Text - Grey', 'txt-brandeis-grey', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
-		new ContentTools.Style('Text - Light Blue', 'txt-brandeis-blue-0', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
+		new ContentTools.Style('Text - Light Blue', 'txt-brandeis-blue-2', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Text - Normal Blue', 'txt-brandeis-blue-1', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
-		new ContentTools.Style('Text - Dark Blue', 'txt-brandeis-blue-2', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
+		new ContentTools.Style('Text - Dark Blue', 'txt-brandeis-blue-0', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Text - Yellow', 'txt-brandeis-yellow', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 
 		new ContentTools.Style('Background - Black', 'bg-brandeis-black', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Background - White', 'bg-brandeis-white', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Background - Grey', 'bg-brandeis-grey', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
-		new ContentTools.Style('Background - Light Blue', 'bg-brandeis-blue-0', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
+		new ContentTools.Style('Background - Light Blue', 'bg-brandeis-blue-2', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Background - Normal Blue', 'bg-brandeis-blue-1', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
-		new ContentTools.Style('Background - Dark Blue', 'bg-brandeis-blue-2', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
+		new ContentTools.Style('Background - Dark Blue', 'bg-brandeis-blue-0', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Background - Yellow', 'bg-brandeis-yellow', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
-
+		
 		new ContentTools.Style('Padding - Top - XS', 'pad-t-5', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Padding - Top - S', 'pad-t-10', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
 		new ContentTools.Style('Padding - Top - M', 'pad-t-15', ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'li', 'ul', 'div', 'img', 'video']),
